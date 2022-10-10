77de4a89ec5905c894bb00b61462e2d45b6eca99
Darwin073100 <edwinegq@outlook.com>
2022-10-09 23:43:07 -0500

    Modificando una entidad

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -3,15 +3,18 @@ const Joi = require('joi');
 const id = Joi.number().integer();
 const password = Joi.string().min(8);
 const email = Joi.string().email();
+const role = Joi.string().min(5);
 
 const createUserSchema = Joi.object({
   password: password.required(),
   email: email.required(),
+  role: role.required(),
 });
 
 const updateUserSchema = Joi.object({
   password,
   email,
+  role,
 });
 
 const getUserSchema = Joi.object({
