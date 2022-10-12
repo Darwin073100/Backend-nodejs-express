793d1ef70514f752dee6e0ed651154aa7846574e
Darwin073100 <edwinegq@outlook.com>
2022-10-11 23:32:52 -0500

    paginacion con limit y offset

--- routes/products.router.js
+++ routes/products.router.js
@@ -5,14 +5,21 @@ const {
   createProductSchema,
   updateProductSchema,
   getProductSchema,
+  queryProductSchema,
 } = require('../schemas/product.schema');
 
 const router = express.Router();
 const service = new ProductsService();
 
-router.get('/', async (req, res) => {
-  const products = await service.findAll();
-  res.json(products);
+router.get('/',
+validatorHandler(queryProductSchema, 'query'),
+async (req, res, next) => {
+  try {
+    const products = await service.findAll(req.query);
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get(
