ee6b9a3e92a200193f9f695483ea896c0d671cef
Edwin Garcia Quiterio <edwinegq@outlook.com>
2023-05-10 22:51:12 -0600

    Implemetando login con Passport.js

--- utils/auth/strategies/local.strategy.js
+++ utils/auth/strategies/local.strategy.js
@@ -4,6 +4,7 @@ const boom = require("@hapi/boom");
 const UserService = require("../../../services/users.service");
 
 const service = new UserService();
+
 const LocalStrategy = new Strategy({
   usernameField: 'email',
   passwordField: 'password'
